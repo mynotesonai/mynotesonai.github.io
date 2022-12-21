9824f8ece90c2872ff24166a6d31f056d511d4a3
pam1202g <pam1202.g@gmail.com>
2022-12-21 12:08:57 +1100

    adding more work on Multivariable Calculus

--- all_files/js/init.js
+++
@@ -1,9 +0,0 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-	 $('.slider').slider({full_width: true});
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
